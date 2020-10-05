5437fcecd247c92ea751d307a83fac373b56aefb
Michael Kirby <kirbyma729@gmail.com>
2020-10-04 23:33:59 -0400

    prettier formatting

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,15 +1,15 @@
-import React, { Fragment, useEffect } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { getGamesByDate } from '../../actions/schedule';
-import { getActiveTeams } from '../../actions/teams';
-import ListOfGames from '../games/ListOfGames';
-import Spinner from '../common/Spinner';
+import React, { Fragment, useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getGamesByDate } from "../../actions/schedule";
+import { getActiveTeams } from "../../actions/teams";
+import ListOfGames from "../games/ListOfGames";
+import Spinner from "../common/Spinner";
 
 const Landing = ({ getGamesByDate, getActiveTeams, schedule }) => {
   useEffect(() => {
     // getGamesByDate();
-    getGamesByDate('2019-04-05', '2019-04-05');
+    getGamesByDate("2020-08-01", "2020-08-02");
     getActiveTeams();
   }, [getGamesByDate, getActiveTeams]);
 
@@ -18,9 +18,9 @@ const Landing = ({ getGamesByDate, getActiveTeams, schedule }) => {
       {schedule.loading ? (
         <Spinner />
       ) : (
-        <div className="container-fluid" style={{ marginTop: '5vh' }}>
+        <div className="container-fluid" style={{ marginTop: "5vh" }}>
           {schedule.dates.length > 0 ? (
-            schedule.dates.map(gamesOnDay => (
+            schedule.dates.map((gamesOnDay) => (
               <div key={gamesOnDay.date} className="row no-gutters">
                 <div key={gamesOnDay.date} className="col-12">
                   <ListOfGames key={gamesOnDay.date} gamesData={gamesOnDay} />
@@ -28,7 +28,7 @@ const Landing = ({ getGamesByDate, getActiveTeams, schedule }) => {
               </div>
             ))
           ) : (
-            <div className="text-center" style={{ marginTop: '15vh' }}>
+            <div className="text-center" style={{ marginTop: "15vh" }}>
               <span className="h5 pill-lg ice">No games today</span>
             </div>
           )}
@@ -41,14 +41,13 @@ const Landing = ({ getGamesByDate, getActiveTeams, schedule }) => {
 Landing.propTypes = {
   getGamesByDate: PropTypes.func.isRequired,
   getActiveTeams: PropTypes.func.isRequired,
-  schedule: PropTypes.object.isRequired
+  schedule: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-  schedule: state.schedule
+const mapStateToProps = (state) => ({
+  schedule: state.schedule,
 });
 
-export default connect(
-  mapStateToProps,
-  { getGamesByDate, getActiveTeams }
-)(Landing);
+export default connect(mapStateToProps, { getGamesByDate, getActiveTeams })(
+  Landing
+);
