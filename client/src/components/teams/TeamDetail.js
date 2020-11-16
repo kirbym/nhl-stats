68c38fbfa9db68975857a7527adbae024f9bae35
Michael Kirby <kirbyma729@gmail.com>
2020-11-15 23:08:06 -0500

    get team id from params with useParams hook

--- client/src/components/teams/TeamDetail.js
+++ client/src/components/teams/TeamDetail.js
@@ -3,11 +3,14 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import TeamLogo from "../logos/TeamLogo";
 import classNames from "classnames";
+import { useParams } from "react-router-dom";
 
 const TeamDetail = () => {
+  let { teamId } = useParams();
+
   return (
     <Fragment>
-      <div>TeamDetail</div>
+      <div>TeamDetail {`${teamId}`}</div>
     </Fragment>
   );
 };
