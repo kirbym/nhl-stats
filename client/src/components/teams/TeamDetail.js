caa94b3636fcd196247d8a0a41a983dbaf221446
Michael Kirby <kirbyma729@gmail.com>
2020-11-18 21:35:29 -0500

    set up base TeamDetail component - requesting action and getting the values back in state

--- client/src/components/teams/TeamDetail.js
+++ client/src/components/teams/TeamDetail.js
@@ -1,20 +1,37 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import TeamLogo from "../logos/TeamLogo";
-import classNames from "classnames";
 import { useParams } from "react-router-dom";
+import classNames from "classnames";
+import TeamLogo from "../logos/TeamLogo";
+import { getTeamById } from "../../actions/teams";
 
-const TeamDetail = () => {
+const TeamDetail = ({ getTeamById, teams: { loading, selectedTeam } }) => {
   let { teamId } = useParams();
+
+  useEffect(() => {
+    getTeamById(teamId);
+  }, [teamId, getTeamById]);
 
   return (
     <Fragment>
-      <div>TeamDetail {`${teamId}`}</div>
+      {selectedTeam === null || selectedTeam === undefined || loading ? (
+        <p>is loading</p>
+      ) : (
+        <Fragment>
+          <div>TeamDetail {`${teamId}`}</div>
+          <div>selected team {selectedTeam.name} </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
 
-TeamDetail.propTypes = {};
+TeamDetail.propTypes = {
+  getTeamById: PropTypes.func.isRequired,
+  teams: PropTypes.object.isRequired,
+};
 
-export default connect(null, null)(TeamDetail);
+const mapStateToProps = (state) => ({ teams: state.teams });
+
+export default connect(mapStateToProps, { getTeamById })(TeamDetail);
